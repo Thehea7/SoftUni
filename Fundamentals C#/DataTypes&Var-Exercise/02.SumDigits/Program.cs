3aa485d31f6ad778116fb2e36aa794c3ec4b8972
Thehea7 <matsevandrey@gmail.com>
2023-10-22 15:56:46 +0000

    Save before I leave the office

---
+++ Fundamentals C#/DataTypes&Var-Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,10 @@
+﻿int input = int.Parse(Console.ReadLine());
+int sum = 0;
+while (input > 0)
+{
+    int lastDigit = input % 10;
+    sum += lastDigit;
+    input /= 10;
+}
+
+Console.WriteLine(sum);
