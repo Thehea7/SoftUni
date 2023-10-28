b9238e9b862711a7794e9fe1fd50a1220fec9c6e
Thehea7 <matsevandrey@gmail.com>
2023-10-28 15:17:00 +0000

    uploading solution to problem 1

---
+++ Fundamentals C#/Arrays-Exercise/01.Train/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nWagon = int.Parse(Console.ReadLine());
+            int sum = 0;
+            string[] output = new string[nWagon];
+
+            for (int i = 0; i < nWagon; i++)
+            {
+                string input = Console.ReadLine();
+                sum += int.Parse(input);
+                output[i] = input;
+
+            }
+            Console.WriteLine(string.Join(" ", output));
+            Console.WriteLine(sum);
+        }
+    }
+}
