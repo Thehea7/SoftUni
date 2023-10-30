6c537bb4e552bbe76ef84990254ab354c5002556
Thehea7 <matsevandrey@gmail.com>
2023-10-30 22:55:10 +0000

    Solution to problem 2

--- Fundamentals C#/Arrays-MoreExercise/02.PascalTriangle/Program.cs
+++ Fundamentals C#/Arrays-MoreExercise/02.PascalTriangle/Program.cs
@@ -4,12 +4,34 @@
     {
         static void Main(string[] args)
         {
-         int n = int.Parse(Console.ReadLine());
-
+            int n = int.Parse(Console.ReadLine());
+            int[] lastArr = new int[1];
             for (int i = 1; i <= n; i++)
             {
+                int j = 1;
                 int[] currentArr = new int[i];
+                currentArr[0] = 1;
+                if (i == 1) { currentArr[0] = 1; }
+                for ( j = 1; j < currentArr.Length; j++)
+                {
+                    if (j == currentArr.Length - 1) 
+                    {
+                        currentArr[j] = 1;
+                        break;
+                    }
+                    currentArr[j] = lastArr[j] + lastArr[j - 1];
+                }
 
+              lastArr = new int[i];
+                for (j = 0; j < lastArr.Length; j++)
+                {
+                    lastArr[j] = currentArr[j];
+                }
+                foreach (var number in currentArr)
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
             }
         }
     }
