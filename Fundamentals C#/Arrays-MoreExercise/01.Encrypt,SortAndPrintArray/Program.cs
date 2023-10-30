aae6200c8d2a378d0f2c9909457f87989d7e6ad0
Thehea7 <matsevandrey@gmail.com>
2023-10-30 15:02:42 +0000

    Soluion to Problem 1

---
+++ Fundamentals C#/Arrays-MoreExercise/01.Encrypt,SortAndPrintArray/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Encrypt_SortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            int[] output = new int[nLines];
+            for (int i = 0; i < nLines; i++)
+            {
+                string input = Console.ReadLine();
+                int sum = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    char a = input[j];
+                    if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
+                    {
+                        sum += a * input.Length;
+                    }
+                    else
+                    {
+                        sum += a / input.Length;
+                    }
+
+                }
+                output[i] = sum;
+
+            }
+            Array.Sort(output);
+            foreach (int i in output)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ Fundamentals C#/Arrays-MoreExercise/02.PascalTriangle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int[] currentArr = new int[i];
+
+            }
+        }
+    }
+}
+
