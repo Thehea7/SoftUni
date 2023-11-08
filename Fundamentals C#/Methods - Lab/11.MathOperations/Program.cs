b8d923dee59cb283e30d3772b2d796d18a74389e
Thehea7 <matsevandrey@gmail.com>
2023-11-08 22:30:10 +0000

    Ready with the Lab

---
+++ Fundamentals C#/Methods - Lab/11.MathOperations/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            char operatorr = char.Parse(Console.ReadLine());
+            double number2  = double.Parse(Console.ReadLine());
+
+            double output = CheckOperatorAndReturnResult(operatorr, number1, number2);
+            Console.WriteLine(output);
+        }
+
+        private static double CheckOperatorAndReturnResult(char operatorr, double number1, double number2)
+        {
+            double result = default;
+            if (operatorr == '/')
+            {
+                result = 1.0 * number1 / number2;
+            }
+            else if (operatorr == '*')
+            {
+                result = number1 * number2;
+            }
+            else if (operatorr == '+')
+            {
+                result = number1 + number2;
+            }
+            else if (operatorr == '-')
+            {
+                result = number1 - number2;
+            }
+
+            return result;
+        }
+    }
+}
