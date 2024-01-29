fb00f946b8ea1a77be7d268db9463713dfbde481
Thehea7 <matsevandrey@gmail.com>
2024-01-29 12:41:23 +0000

    SetsAndDictionariesAdvanced-Exercise - Completed

--- Advanced C#/SetsAndDictionariesAdvanced-Exercise/09.SoftUniExamResults/Program.cs
+++ Advanced C#/SetsAndDictionariesAdvanced-Exercise/09.SoftUniExamResults/Program.cs
@@ -2,9 +2,58 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> courses = new Dictionary<string, int>();
+            Dictionary<string, int> students = new Dictionary<string,int>();
+
+            string commands;
+            while ((commands = Console.ReadLine()) != "exam finished")
+            {
+                string[] arguments = commands.Split("-", StringSplitOptions.RemoveEmptyEntries);
+                string studentName = arguments[0];
+                string command = arguments[1];
+                if (command == "banned")
+                {
+                    students.Remove(studentName);
+                }
+                else
+                {
+                    string course = command;
+                    int points = int.Parse(arguments[2]);
+                    if (!courses.ContainsKey(course))
+                    {
+                        courses.Add(course, 0);
+                    }
+                    courses[course]++;
+
+                    if (!students.ContainsKey(studentName))
+                    {
+                        students.Add(studentName, points);
+                    }
+
+                    if (students[studentName] < points)
+                    {
+                        students[studentName] = points;
+                    }
+                }
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var (student, score) in students
+                         .OrderByDescending(x => x.Value)
+                         .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{student} | {score}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var (course, submissions) in courses
+                         .OrderByDescending(x => x.Value)
+                         .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{course} - {submissions}");
+            }
+
         }
     }
 }
---
+++ Advanced C#/SetsAndDictionariesAdvanced-Exercise/10.ForceBook/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _10.ForceBook
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, HashSet<string>> sides = new Dictionary<string, HashSet<string>>();
+
+
+
+            string commands;
+            while ((commands = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (commands.Contains(" | "))
+                {
+                    string[] arguments = commands.Split(" | ", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                    string side = arguments[0];
+                    string name = arguments[1];
+                    if (!sides.ContainsKey(side))
+                    {
+                        sides.Add(side, new HashSet<string>());
+                    }
+
+                    if (NewForceUser(name, sides))
+                    {
+                        sides[side].Add(name);
+                    }
+
+                }
+                else
+                {
+                    string[] arguments = commands.Split(" -> ", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                    string side = arguments[1];
+                    string name = arguments[0];
+                    if (!sides.ContainsKey(side))
+                    {
+                        sides.Add(side, new HashSet<string>());
+                    }
+                    if (NewForceUser(name, sides) || !sides[side].Contains(name))
+                    {
+                        sides = DeleteUser(name, sides);
+                        sides[side].Add(name);
+
+                        Console.WriteLine($"{name} joins the {side} side!");
+                    }
+                }
+
+            }
+
+            foreach (var (side, names) in sides
+                         .OrderByDescending(x => x.Value.Count)
+                         .ThenBy(x => x.Key))
+            {
+                if (names.Count > 0)
+                {
+
+                    Console.WriteLine($"Side: {side}, Members: {names.Count}");
+                    foreach (var name in names
+                                 .OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {name}");
+                    }
+                }
+            }
+
+
+        }
+
+        private static bool NewForceUser(string name, Dictionary<string, HashSet<string>> sides)
+        {
+            foreach (var side in sides)
+            {
+                if (side.Value.Contains(name))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Dictionary<string, HashSet<string>> DeleteUser(string name, Dictionary<string, HashSet<string>> sides)
+        {
+            sides = sides.Where(x => !x.Value.Contains(name)).ToDictionary(d => d.Key, d => d.Value);
+            return sides;
+        }
+    }
+}
+
