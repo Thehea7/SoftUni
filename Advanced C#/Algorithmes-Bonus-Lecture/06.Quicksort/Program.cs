06fff4a3aada65483ccaedc439a24cc19f7ddeeb
Thehea7 <matsevandrey@gmail.com>
2024-03-28 20:51:57 +0000

    Basic Algorithms - Exercise

---
+++ Advanced C#/Algorithmes-Bonus-Lecture/06.Quicksort/Program.cs
@@ -0,0 +1,52 @@
+﻿//var numbers = new[] { 1, 5, 0, 0, 3, 9,125 };
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+SortArray(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
+
+static void SortArray(int[] numbers)
+{
+    QuickSort(numbers, 0, numbers.Length - 1);
+}
+
+static void QuickSort(int[] numbers, int begining, int finish)
+{
+    int startIndex = begining;
+    int endIndex = finish;
+
+    var pivot = numbers[(begining + finish) / 2] ;
+
+    while (startIndex <= endIndex)
+    {
+        while (numbers[startIndex] < pivot)
+        {
+            startIndex++;
+        }
+
+        while (numbers[endIndex] > pivot)
+        {
+            endIndex--;
+        }
+
+        if (startIndex <= endIndex)
+        {
+            (numbers[startIndex], numbers[endIndex]) = (numbers[endIndex], numbers[startIndex]);
+            startIndex++;
+            endIndex--;
+        }
+    }
+
+    if (begining < endIndex)
+    {
+        QuickSort(numbers, begining, endIndex);
+    }
+
+    if (startIndex < finish)
+    {
+        QuickSort(numbers, startIndex, finish);
+    }
+}
---
+++ Advanced C#/Algorithmes-Bonus-Lecture/Binary Search/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Linq.Expressions;
+
+
+int[] sortedArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int element = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetIndex(sortedArray, element));
+
+static int GetIndex(int[] sortedArray, int element)
+{
+    int index = FindIndex(sortedArray, element, 0, sortedArray.Length - 1);
+    return index;
+}
+static int FindIndex(int[] array, int element, int startIndex, int endIndex)
+{
+    int index = -1;
+
+
+    //Random random = new Random();
+    //int randomIndex = random.Next(startIndex, endIndex);
+    int randomIndex = (endIndex - startIndex) / 2 + startIndex;
+
+    if (array[randomIndex] == element)
+    {
+        index = randomIndex;
+    }
+
+    if (array[randomIndex] < element && endIndex - randomIndex > 0)
+    {
+        index = FindIndex(array, element, randomIndex+1, endIndex);
+    }
+
+    if (array[randomIndex] > element && randomIndex - startIndex >0 )
+    {
+        index = FindIndex(array, element, startIndex, randomIndex-1);
+    }
+
+    
+
+    return index;
+}
