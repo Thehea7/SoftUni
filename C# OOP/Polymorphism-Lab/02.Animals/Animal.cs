63787375a9ba17e0dcded7d43035a6cfa11cbccc
Thehea7 <matsevandrey@gmail.com>
2024-04-09 20:06:09 +0000

    Polymorphism - Lab - Ready

---
+++ C# OOP/Polymorphism-Lab/02.Animals/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected string name;
+        protected string favouriteFood;
+        protected Animal(string name, string favouriteFood)
+        {
+            this.name = name;
+            this.favouriteFood = favouriteFood;
+        }
+
+        public abstract string ExplainSelf();
+    }
+}
---
+++ C# OOP/Polymorphism-Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public double Height { get; init; }
+        public double Width { get; init; }
+
+        public override double CalculatePerimeter()
+        {
+            return 2*(Height + Width);
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+    }
+}
