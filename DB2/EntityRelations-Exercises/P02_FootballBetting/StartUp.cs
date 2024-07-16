50e8e25667b73d9128074ace22060e1b25bdbced
Andrey.M <matsevandrey@gmail.com>
2024-07-17 00:05:11 +0300

    starting problem 2

---
+++ DB2/EntityRelations-Exercises/P02_FootballBetting/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
